9aefe2946a626d43bf5054eb8ade1e5ccf3f78b9
yapiko5505 <bloomingjewels7021@gmail.com>
2021-07-14 08:29:35 +0900

    twitterclone1

---
+++ Controllers/home.php
@@ -0,0 +1,26 @@
+<?php
+// ホームコントローラー
+
+// 設定を読み込み
+include_once '../config.php';
+// 便利な関数を読み込み
+include_once '../util.php';
+
+// ツイートデータ操作モデルを読み込む
+include_once '../Models/tweets.php';
+
+// ログインしているか
+$user = getUserSession();
+if (!$user) {
+    // ログインしていない
+    header('Location:' . HOME_URL .'Controllers/sign-in.php');
+    exit;
+}
+
+// 画面表示
+$view_user = $user;
+
+// ツイート一覧作成
+$view_tweets = findTweets($user); 
+
+include_once '../views/home.php';
---
+++ views/post.php
@@ -0,0 +1,46 @@
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    
+    <?php include_once('../views/common/head.php'); ?>
+    <title>つぶやく画面/twitterクローン</title>
+    <meta name="discription" content="つぶやく画面です">
+
+</head>
+<body class="home">
+    <div class="container">
+        <?php include_once('../views/common/side.php'); ?>
+        <div class="main">
+            <div class="main-header">
+                <h1>つぶやく</h1>
+                
+            </div>
+            <div class="tweet-post">
+                <div class="my-icon">
+                    <img src="<?php echo htmlspecialchars($view_user['image_path']); ?>" alt="">
+                </div>
+                <div class="input-area">
+                    <form action="post.php" method="post" enctype="multipart/form-data">
+                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
+                        <div class="bottom-area">
+                            <div class="mb-0">
+                                <input type="file" name="image" class="form-control form-control-sm">
+                            </div>
+                            <button class="btn" type="submit">つぶやく</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+            <div class="ditch"></div>
+
+
+        </div>
+    </div>
+
+    <?php include_once('../views/common/foot.php'); ?>
+</body>
+
+</html>
